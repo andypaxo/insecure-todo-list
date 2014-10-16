724c50eb7de62ae3f4820ee32259f0f9f6a32d75
andypaxo <andy@andypaxo.net>
2014-10-16 06:57:18 -0600

    Split auth functions out into seperate module

---
+++ auth.js
@@ -0,0 +1,50 @@
+(function () {
+	var session = require('express-session');
+	var passport = require('passport');
+	var AuthStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+	var attachTo = function (app) {
+		app.use(session({
+			secret: 'wonky harpsichord',
+			resave: true,
+			saveUninitialized: true
+		}));
+		app.use(passport.initialize());
+		app.use(passport.session());
+
+		app.get('/auth', passport.authenticate('google', { scope: 'profile' }));
+
+		app.get('/auth/return', passport.authenticate('google',
+			{
+				successRedirect: '/',
+				failureRedirect: '/'
+			}));
+
+		app.get('/logout', function(req, res){
+		  req.logout();
+		  res.redirect('/');
+		});
+	};
+
+
+	passport.serializeUser(function(user, done) {
+	  done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done) {
+		done(null, id);
+	});
+
+	passport.use(new AuthStrategy(
+		{
+		    clientID: process.env.google_client_id,
+		    clientSecret: process.env.google_client_secret,
+			callbackURL: process.env.google_callback_url
+		},
+		function(token, tokenSecret, profile, done) {
+			//console.log(arguments);
+			done(null, profile);
+		}));
+
+	exports.attachTo = attachTo;
+})();
--- index.js
+++ index.js
@@ -1,55 +1,13 @@
 var express = require('express');
-var session = require('express-session');
-var passport = require('passport');
-
 var app = express();
-var AuthStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-app.use(session({
-	secret: 'wonky harpsichord',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-	done(null, id);
-});
-
-passport.use(new AuthStrategy(
-	{
-	    clientID: process.env.google_client_id,
-	    clientSecret: process.env.google_client_secret,
-		callbackURL: process.env.google_callback_url
-	},
-	function(token, tokenSecret, profile, done) {
-		//console.log(arguments);
-		done(null, profile);
-	}));
+var auth = require('./auth');
+auth.attachTo(app);
 
 app.get('/', function (req, res) {
 	res.send(req.user ?
 		'Signed in as user ' + req.user :
 		'<a href="/auth">Sign In with Google</a>');
-});
-
-app.get('/auth', passport.authenticate('google', { scope: 'profile' }));
-
-app.get('/auth/return', passport.authenticate('google',
-	{
-		successRedirect: '/',
-		failureRedirect: '/'
-	}));
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
 });
 
 var port = process.env.PORT || 4242;
