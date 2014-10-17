56f28aa27878b869fdfe983995cc76bb73b49c5a
andypaxo <andy@andypaxo.net>
2014-10-16 21:21:24 -0600

    Start to build out DB

--- db.js
+++ db.js
@@ -3,10 +3,19 @@
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     if (err) {
-      console.error(err);
-    } else {
-      console.log('Connected to postgres successfully');
-    }
+    	throw err;
+
+	console.log('Connected to postgres successfully');
+	client.query('SELECT * FROM pg_catalog.tables WHERE tablename = pb_users', function (err, result) {
+		if (err)
+			throw err;
+
+		if (result.rows.length)
+			return;
+
+		// TODO : Create database
+		client.query();
+	});
   });
 
 })();
