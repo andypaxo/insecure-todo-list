b2792f97effadf76780f709e64321b67dcac3c5c
andypaxo <andy@andypaxo.net>
2014-10-19 09:17:08 -0600

    Render HTML views (using Jade)

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'jade');
 
-var auth = require('./auth');
+var auth = require('./modules/auth');
 auth.attachTo(app);
 
-var db = require('./db');
+var db = require('./modules/db');
 
 app.get('/', function (req, res) {
-	res.send(req.user ?
-		'Signed in as ' + req.user.name :
-		'<a href="/auth">Sign In with Google</a>');
+	var username = req.user ? req.user.name : 'nobody';
+	res.render('index.jade', { username : username });
+	// res.send(req.user ?
+	// 	'Signed in as ' + req.user.name :
+	// 	'<a href="/auth">Sign In with Google</a>');
 });
 
 var port = process.env.PORT || 4242;
